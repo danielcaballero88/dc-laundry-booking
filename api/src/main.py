625a597717dd5ba28d3aa73a2dc3a1c165dc9acb
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-29 18:51:16 +0700

    API: Move auth routes to separate module

--- api/src/main.py
+++ api/src/main.py
@@ -1,9 +1,8 @@
 """Main module for the API."""
-import auth
 import fastapi as fa
+
 import mongodb
-from auth.models import user
-from fastapi import security as fas
+from routers import auth_router
 
 app = fa.FastAPI()
 
@@ -20,44 +19,10 @@ async def shutdown():
     mongodb.mongo_db_conn.close_client()
 
 
-user_coll = mongodb.mongo_db_conn.get_coll(
-    db_name="dc_laundry_booking", coll_name="auth"
-)
-
-authenticator = auth.Authenticator(user_coll=user_coll)
-
-
 @app.get("/")
 async def hello_world():
     """Returns Hello World."""
     return {"Hello": "World"}
 
 
-@app.post("/register")
-async def register(new_user: user.UserRegister):
-    """Registers a new user."""
-    new_user_id = authenticator.register_new_user(new_user)
-    return {"id_": new_user_id}
-
-
-@app.post("/login")
-async def login(form_data: fas.OAuth2PasswordRequestForm = fa.Depends()):
-    """Logins a user (using username and password) and returns a token."""
-    token = authenticator.authenticate_user(form_data.username, form_data.password)
-    return token
-
-
-@app.get("/users/me")
-async def get_users_me(
-    user_me: user.UserBase = fa.Depends(authenticator.get_current_user),
-):
-    """Get the user making the request.
-
-    GET /users/me gets the data of the user associated with the bearer
-    token passed for authentication in the headers.
-
-    Dependency injection:
-    - user.get_current_active_user(UserBase) -> UserBase
-        - user.get_current_user(token) -> UserBase
-    """
-    return user_me
+app.include_router(auth_router.router)
---
+++ api/src/routers/auth_router.py
@@ -0,0 +1,46 @@
+"""Routes for authentication."""
+import fastapi as fa
+from fastapi import security as fas
+
+import auth
+import mongodb
+from auth.models import user
+
+user_coll = mongodb.mongo_db_conn.get_coll(
+    db_name="dc_laundry_booking", coll_name="auth"
+)
+
+authenticator = auth.Authenticator(user_coll=user_coll)
+
+
+router = fa.APIRouter(tags=["auth"])
+
+
+@router.post("/register")
+async def register(new_user: user.UserRegister):
+    """Registers a new user."""
+    new_user_id = authenticator.register_new_user(new_user)
+    return {"id_": new_user_id}
+
+
+@router.post("/login")
+async def login(form_data: fas.OAuth2PasswordRequestForm = fa.Depends()):
+    """Logins a user (using username and password) and returns a token."""
+    token = authenticator.authenticate_user(form_data.username, form_data.password)
+    return token
+
+
+@router.get("/users/me")
+async def get_users_me(
+    user_me: user.UserBase = fa.Depends(authenticator.get_current_user),
+):
+    """Get the user making the request.
+
+    GET /users/me gets the data of the user associated with the bearer
+    token passed for authentication in the headers.
+
+    Dependency injection:
+    - user.get_current_active_user(UserBase) -> UserBase
+        - user.get_current_user(token) -> UserBase
+    """
+    return user_me
