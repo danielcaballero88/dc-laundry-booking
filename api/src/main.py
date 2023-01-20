847bbcc407ca67946a8e5de7e918e47f8719e300
Daniel Caballero <danielcaballero88@gmail.com>
2023-01-20 17:24:48 +0800

    API: Small format fix with black

--- api/src/main.py
+++ api/src/main.py
@@ -17,9 +17,10 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST", "GET"],
-		allow_headers=["*"],
+    allow_headers=["*"],
     max_age=3600,
 )
+
 
 @app.on_event("startup")
 async def startup():
