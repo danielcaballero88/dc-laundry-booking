3560f37f21fc9e0b9c588911a463c93c23236820
Daniel Caballero <danielcaballero88@gmail.com>
2023-01-22 09:21:28 +0800

    API: Add DELETE to CORS policy

--- api/src/main.py
+++ api/src/main.py
@@ -16,7 +16,11 @@ app.add_middleware(
     fa_cors.CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET"],
+    allow_methods=[
+        "POST",
+        "GET",
+        "DELETE",
+    ],
     allow_headers=["*"],
     max_age=3600,
 )
